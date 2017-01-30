19deedac72a777ad0f2eaaa2c81f1fcd6266b6c1
CathyCorrea <cathy.correa@versionone.com>
2017-01-30 16:39:58 -0500

    Use remote tags instead of pulling down each branch

--- PointReleaseLog/git_commits.sh
+++ PointReleaseLog/git_commits.sh
@@ -1,26 +1,23 @@
-BRANCH="15.3"
-printf "Finding workitems in latest $BRANCH build since last point release\n" >point_release_commits.txt
-git checkout $BRANCH
-git pull --ff-only
-TAG=$(git describe --abbrev=0 --tags)
-echo $TAG
-printf "git log $TAG..$BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{5}' | sort |uniq\n" >>point_release_commits.txt
-git log $TAG..$BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{5}' | sort |uniq >>point_release_commits.txt 2>&1
+cd "/c/Development/Core"
 
-BRANCH="16.0"
-printf "\nFinding workitems in latest $BRANCH build since last point release\n" >>point_release_commits.txt
-git checkout $BRANCH
-git pull --ff-only
-TAG=$(git describe --abbrev=0 --tags)
+git fetch
+BRANCH="origin/16.2"
+printf "\nFinding workitems in latest $BRANCH build since last point release\n" >point_release_commits.txt
+TAG=$(git describe $BRANCH --abbrev=0 --tags)
 echo $TAG
-printf "git log $TAG..$BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{5}' | sort |uniq\n" >>point_release_commits.txt
-git log $TAG..$BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{5}' | sort |uniq >>point_release_commits.txt 2>&1
+printf "git log $TAG..$BRANCH $BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{4,6}' | sort |uniq\n" >>point_release_commits.txt
+git log $TAG..$BRANCH $BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{4,6}' | sort |uniq >>point_release_commits.txt 2>&1
 
-BRANCH="16.1"
+BRANCH="origin/16.3"
 printf "\nFinding workitems in latest $BRANCH build since last point release\n" >>point_release_commits.txt
-git checkout $BRANCH
-git pull --ff-only
-TAG=$(git describe --abbrev=0 --tags)
+TAG=$(git describe $BRANCH --abbrev=0 --tags)
 echo $TAG
-printf "git log $TAG..$BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{5}' | sort |uniq\n" >>point_release_commits.txt
-git log $TAG..$BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{5}' | sort |uniq >>point_release_commits.txt 2>&1
+printf "git log $TAG..$BRANCH $BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{4,6}' | sort |uniq\n" >>point_release_commits.txt
+git log $TAG..$BRANCH $BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{4,6}' | sort |uniq >>point_release_commits.txt 2>&1
+
+BRANCH="origin/17.0"
+printf "\nFinding workitems in latest $BRANCH build since last point release\n" >>point_release_commits.txt
+TAG=$(git describe $BRANCH --abbrev=0 --tags)
+echo $TAG
+printf "git log $TAG..$BRANCH $BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{4,6}' | sort |uniq\n" >>point_release_commits.txt
+git log $TAG..$BRANCH $BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{4,6}' | sort |uniq >>point_release_commits.txt 2>&1
