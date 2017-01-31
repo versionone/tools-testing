2272f6fa03f070ad0f8134fe7384f690debd46ab
CathyCorrea <cathy.correa@versionone.com>
2017-01-30 21:54:53 -0500

    parameterize script to pass in branches to inspect

--- PointReleaseLog/git_commits.sh
+++ PointReleaseLog/git_commits.sh
@@ -1,23 +1,18 @@
-cd "/c/Development/Core"
-
+#cd "/c/Development/Core"
 git fetch
-BRANCH="origin/16.2"
-printf "\nFinding workitems in latest $BRANCH build since last point release\n" >point_release_commits.txt
-TAG=$(git describe $BRANCH --abbrev=0 --tags)
-echo $TAG
-printf "git log $TAG..$BRANCH $BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{4,6}' | sort |uniq\n" >>point_release_commits.txt
-git log $TAG..$BRANCH $BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{4,6}' | sort |uniq >>point_release_commits.txt 2>&1
+printf "" > point_release_commits.txt
 
-BRANCH="origin/16.3"
-printf "\nFinding workitems in latest $BRANCH build since last point release\n" >>point_release_commits.txt
-TAG=$(git describe $BRANCH --abbrev=0 --tags)
-echo $TAG
-printf "git log $TAG..$BRANCH $BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{4,6}' | sort |uniq\n" >>point_release_commits.txt
-git log $TAG..$BRANCH $BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{4,6}' | sort |uniq >>point_release_commits.txt 2>&1
+function getCommitsSinceLastTag ()
+{
+	BRANCH=$1
+	printf "\nFinding workitems in latest $BRANCH build since last point release\n" >>point_release_commits.txt
+	TAG=$(git describe $BRANCH --abbrev=0 --tags)
+	echo $TAG
+	printf "git log $TAG..$BRANCH $BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{4,6}' | sort |uniq\n" >>point_release_commits.txt
+	git log $TAG..$BRANCH $BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{4,6}' | sort |uniq >>point_release_commits.txt 2>&1
+}
 
-BRANCH="origin/17.0"
-printf "\nFinding workitems in latest $BRANCH build since last point release\n" >>point_release_commits.txt
-TAG=$(git describe $BRANCH --abbrev=0 --tags)
-echo $TAG
-printf "git log $TAG..$BRANCH $BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{4,6}' | sort |uniq\n" >>point_release_commits.txt
-git log $TAG..$BRANCH $BRANCH | grep -o -E '[A-Z]{1,2}-[0-9]{4,6}' | sort |uniq >>point_release_commits.txt 2>&1
+for version in "$@"
+do
+    getCommitsSinceLastTag "origin/$version"
+done
